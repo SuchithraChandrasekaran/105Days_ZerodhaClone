26e782e6fc2d5fbca62263b0591962edd836138a
Suchithra Chandrasekaran <suchithrac@gmail.com>
2025-01-20 21:30:22 +0530

    Created folders Day 1 to Day 105

--- create_folders.sh
+++ create_folders.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Loop to create folders from Day 1 to Day 105
-for i in {1..105}
-do
-  folder_name="Day $i"
-  rm -rf  "$folder_name"
-done
+#for i in {1..105}
+#do
+#  folder_name="Day $i"
+#  rm -rf  "$folder_name"
+#done
 
 # Loop to create folders from Day 1 to Day 105
-for i in {1..105}
-do
-  folder_name="Day_$i"
-  mkdir "$folder_name"
-done
+#for i in {1..105}
+#do
+# folder_name="Day_$i"
+#  mkdir "$folder_name"
+#done
 
 for i in {1..105}
 do
-  if [ -d "Day $i" ]; then  # Check if the folder exists
-    touch "Day $i/.gitkeep"  # Create the .gitkeep file
-    git add "Day $i/.gitkeep"  # Stage the .gitkeep file for commit
+  if [ -d "Day_$i" ]; then  # Check if the folder exists
+    touch "Day_$i/.gitkeep"  # Create the .gitkeep file
+    git add "Day_$i/.gitkeep"  # Stage the .gitkeep file for commit
   fi
 done
 
